a3dfdec3e3104b6d1600bf11a9aed746bb99f3c4
Legacy1Q <mdsims2@outlook.com>
2023-06-05 23:51:26 -0400

    first lines of code; donut count + adder

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const container = (document.querySelector('.container').innerText =
-  'mmmmmmmmm doughnuts.... ahhhhhhh!');
+let dountCount = 0;
+
+const donutCounter = document.createElement("h1");
+donutCounter.innerText = dountCount;
+document.querySelector(".container").appendChild(donutCounter);
+
+const donutButton = document.createElement("button");
+donutButton.innerText = "Donut Maker";
+document.querySelector(".container").appendChild(donutButton);
+
+donutButton.addEventListener("click", () => {
+  dountCount++;
+  donutCounter.innerText = dountCount;
+});
+
